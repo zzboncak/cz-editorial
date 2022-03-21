964922ff742cf68a00b395f55669987afbd4daf8
Zack Zboncak <Zack.Zboncak@poly.com>
2022-03-21 09:28:12 -0500

    added favicons

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,6 +19,27 @@ export const links: LinksFunction = () => {
     {
       rel: "stylesheet",
       href: landingStyles
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png"
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png"
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png"
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest"
     }
   ];
 };
