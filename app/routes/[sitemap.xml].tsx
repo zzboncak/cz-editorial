31b755cb378859d11ffa3442110a2ec5eb201f73
zzboncak <zachary.zboncak@gmail.com>
2022-04-13 14:30:05 -0500

    added basic sitemap

---
+++ app/routes/[sitemap.xml].tsx
@@ -0,0 +1,13 @@
+export function loader() {
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://www.czeditorial.com/</loc>
+      <priority>1.0</priority>
+      <changefreq>daily</changefreq>
+    </url>
+    </urlset>`,
+    { headers: { "Content-Type": "text/xml" } }
+  );
+}
